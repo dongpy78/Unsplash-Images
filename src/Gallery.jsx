13b4e05bf23fd124402e6934e2bfa89676138b3a
Van Dong <vandong78py@gmail.com>
2024-11-04 14:13:47 +0700

    Render Images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -10,12 +10,57 @@ function Gallery() {
     queryKey: ["images"], //! Tên khóa để xác định query này
     queryFn: async () => {
       const result = await axios.get(url);
+      console.log(result);
 
       return result.data;
     },
   });
-  console.log(response);
-  return <div></div>;
+
+  //! isLoading là một thuộc tính do ReactQuery cung cấp
+  if (response.isLoading) {
+    //! Kiểm tra trạng thái Loading
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (response.isError) {
+    //! Kiểm tra nếu có lỗi xảy ra
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+  const results = response.data.results; //! .results là mảng API trả về
+  console.log(results);
+  if (results.length < 1) {
+    //! Kiểm tra nếu không có kết quả nào
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+
+  //! Trả về kết quả, hiển thị hình ảnh
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 }
 
 export default Gallery;
