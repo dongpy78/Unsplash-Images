211c3ca774db2882a1796356e296bf233a4e2d85
Van Dong <vandong78py@gmail.com>
2024-11-04 13:52:35 +0700

    Setup Query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=PHGXwEbxNBOIneyc54DMWU_DtnVYSPOW3oWF7kZ49ik&query=office";
+
 function Gallery() {
+  //! Sử dụng useQuery để lấy dữ liệu từ API
+  const response = useQuery({
+    queryKey: ["images"], //! Tên khóa để xác định query này
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+  console.log(response);
   return <div></div>;
 }
 
